bca85e9a18251d0ca4cae7270c71f790d140a541
HelenHMaher <helenharlanmaher@gmail.com>
2020-04-08 20:23:10 +0200

    figuring out formula logic

--- src/App.js
+++ src/App.js
@@ -1,53 +1,61 @@
 import React, { useState } from "react";
 import "./App.css";
 import Buttons from "./Buttons";
-import { Display } from "./Display";
+import Display from "./Display";
 
 function App() {
   const [currentVal, setCurrentVal] = useState("0");
   const [currentsign, setCurrentSign] = useState("positive");
   const [lastOperator, setLastOperator] = useState("");
   const [total, setTotal] = useState("");
-  const [lastClicked, setLastClicked] = useState("");
+  const [evaluated, setEvaluated] = useState(false);
   const [error, setError] = useState(false);
+  const [formula, setFormula] = useState("");
 
   const isOperator = /[x/+-]/;
+  const endsWithOperator = /[x/+-/]$/;
+
+  const flashError = (error) => {
+    error = true;
+    setTimeout((error = !error), 3000);
+  };
 
   const handleClear = (e) => {
     setCurrentVal("0");
     setCurrentSign("positive");
-    setLastOperator("");
     setTotal("");
-    setLastClicked("");
+    setEvaluated("");
+    setFormula("");
   };
   const handleNumbers = (e) => {
     const value = e.target.value;
-    setCurrentVal(
-      currentVal === "0" || isOperator.test(currentVal) || lastClicked === "="
-        ? value
-        : currentVal + value
-    );
-    setLastClicked(value);
+    setFormula(formula + value);
+    setEvaluated(false);
+    currentVal === "0" || isOperator.test(currentVal) || evaluated
+      ? setCurrentVal(value)
+      : setCurrentVal(currentVal + value);
   };
   const handleEvaluate = (e) => {
     isOperator.test(currentVal)
-      ? setCurrentVal(total)
-      : setCurrentVal(total + lastOperator + currentVal);
-    setTotal("");
-    setLastClicked("=");
+      ? setCurrentVal(formula)
+      : setCurrentVal(formula + currentVal);
+    setEvaluated(true);
   };
   const handleDecimal = (e) => {
     const value = e.target.value;
-    /./.text(currentVal) ? setError(true) : setCurrentVal(currentVal + value);
+    /./.test(currentVal)
+      ? flashError
+      : setCurrentVal(currentVal + value) && setFormula(formula + value);
   };
   const handleOperators = (e) => {
     const value = e.target.value;
-    !isOperator.test(currentVal) &&
-      setTotal(total !== "" ? total + lastOperator + currentVal : currentVal);
-    setLastOperator(value);
-    setLastClicked(value);
+    !endsWithOperator.test(formula) || !isOperator.test(currentVal)
+      ? setFormula(formula + value)
+      : setFormula(formula.slice(0, formula.length - 1) + value);
     setCurrentVal(value);
   };
+
+  const Error = (props) => {};
 
   return (
     <div className="App">
@@ -55,7 +63,8 @@ function App() {
         <p>Calculator</p>
       </header>
       <div className="calculator">
-        <Display currentValue={currentVal} />
+        <Error id="error" className="error" flasherror={flashError} />
+        <Display currentValue={currentVal} formula={formula} />
         <Buttons
           clear={handleClear}
           numbers={handleNumbers}
--- src/Display/index.js
+++ src/Display/index.js
@@ -3,7 +3,14 @@ import React from "react";
 export const Display = (props) => {
   return (
     <div className="display" id="display">
-      {props.currentValue}
+      <div className="formula" id="formula">
+        Formula: {props.formula}
+      </div>
+      <div clasName="current-display" id="current-display">
+        CurrentValue: {props.currentValue}
+      </div>
     </div>
   );
 };
+
+export default Display;
