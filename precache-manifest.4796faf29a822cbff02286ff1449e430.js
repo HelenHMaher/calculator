17d3e6327d64ba89bb450289b769e3c7739fee9e
HelenHMaher <helenharlanmaher@gmail.com>
2020-04-09 18:29:12 +0200

    Updates

---
+++ precache-manifest.4796faf29a822cbff02286ff1449e430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b4e05ef60d467eecaec163f019ebb2e",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "d6d9af967c0c19cbc396",
+    "url": "/calculator/static/css/main.42e13a6e.chunk.css"
+  },
+  {
+    "revision": "c10192d88c63e3a8ccda",
+    "url": "/calculator/static/js/2.b1a0bd4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.b1a0bd4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6d9af967c0c19cbc396",
+    "url": "/calculator/static/js/main.b727f377.chunk.js"
+  },
+  {
+    "revision": "548babfea1b067596145",
+    "url": "/calculator/static/js/runtime-main.92c42aea.js"
+  }
+]);
